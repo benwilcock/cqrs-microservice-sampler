7ba5574b58ae5c5c58177579d332460c6c3056f5
benwilcock <benwilcock@gmail.com>
2016-07-01 15:15:08 +0100

    updated the docker-compose file as i'm about to start uploading the latest images

--- docker-push.sh
+++ docker-push.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker push benwilcock/config-service:latest
 docker push benwilcock/discovery-service:latest
+docker push benwilcock/gateway-service:latest
 docker push benwilcock/product-query-side:latest
 docker push benwilcock/product-command-side:latest
