7daf84809db7d58664fdbb277c31bfed4d885b90
Pankesh Contractor <pankesh_contractor@cable.comcast.com>
2018-10-02 23:01:24 -0400

    Enabling query side event sourcing

--- query-side/src/main/java/com/pankesh/productquery/ReplayRestController.java
+++ query-side/src/main/java/com/pankesh/productquery/ReplayRestController.java
@@ -1,10 +1,11 @@
 package com.pankesh.productquery;
 
-import java.util.function.Supplier;
-
 import javax.servlet.http.HttpServletResponse;
 
 import org.axonframework.config.EventProcessingConfiguration;
+import org.axonframework.eventhandling.TrackingEventProcessor;
+import org.axonframework.kafka.eventhandling.consumer.KafkaTrackingToken;
+import org.axonframework.messaging.StreamableMessageSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/queries")
 public class ReplayRestController {
 
     private static final Logger LOG = LoggerFactory.getLogger(ReplayRestController.class);
@@ -26,10 +26,15 @@ public class ReplayRestController {
     public void replay(@PathVariable(value = "group") String group, HttpServletResponse response) {
 
         LOG.debug("Start replaying...");
-        final Supplier<? extends RuntimeException> notFoundSupplier = () -> new IllegalArgumentException(
-                "Processor " + group + " not registered.");
-        this.eventProcessingConfiguration.eventProcessor(group).orElseThrow(notFoundSupplier).shutDown();
-        this.eventProcessingConfiguration.eventProcessor(group).orElseThrow(notFoundSupplier).start();
-
+//        final Supplier<? extends RuntimeException> notFoundSupplier = () -> new IllegalArgumentException(
+//                "Processor " + group + " not registered.");
+//        this.eventProcessingConfiguration.eventProcessor(group).orElseThrow(notFoundSupplier).shutDown();
+//        this.eventProcessingConfiguration.eventProcessor(group).orElseThrow(notFoundSupplier).start();
+        this.eventProcessingConfiguration.eventProcessorByProcessingGroup(group, TrackingEventProcessor.class)
+        .ifPresent(trackingEventProcessor -> {
+            trackingEventProcessor.shutDown();
+            trackingEventProcessor.resetTokens();
+            trackingEventProcessor.start();
+        });
     }
 }
